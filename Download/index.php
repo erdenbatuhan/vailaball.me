384dc7c9f21ea34a369d991b132f80b4184990b3
erdenbatuhan <erden.batuhan@gmail.com>
2016-09-07 04:28:06 +0300

    style improvements

--- Download/index.php
+++ Download/index.php
@@ -9,12 +9,13 @@
     <script src="js/bootstrap.js"></script>
 </head>
 <body style="background-color: whitesmoke;">
-    <nav class="navbar navbar-light bg-primary" style="border-radius: 0px; border-bottom-style: groove; border-bottom-color: black; border-bottom-width: 3px;">
-        <div class="container">
-            <a class="navbar-brand" style="color: whitesmoke;" href="http://vailaball.me"> A Vaila Ball </a>
+    <nav class="navbar navbar-light navbar-fixed-top bg-primary" style="border-radius: 0px; border-bottom-style: groove; border-bottom-color: black; border-bottom-width: 3px;">
+        <div class="container-fluid">
+            <a class="navbar-brand" style="color: whitesmoke;" href="http://vailaball.me"><h3>A Vai<span>la Ball</span></h3></a>
         </div>
     </nav>
     <div class="container text-center">
+        <br><br><br><br>
         <h1 style="color: darkred;"> A Vaila Ball Download Links </h1>
         <br>
         <h4> IMPORTANT: If you need an update, you'll need to download the new version and delete the old one from your device. </h4>
@@ -45,7 +46,15 @@
             <a href="not_available.php" target="_self" class="btn btn-danger" role="button" aria-pressed="true"> A Vaila Ball 1.0.3 </a>
         </div>      
         <hr style="border-top: 1px solid #3c763d;">
+        <br><br><br><br><br>
     </div>
+    <nav class="navbar navbar-light navbar-fixed-bottom bg-danger footer" style="border-radius: 0px; padding-top: 10px;">
+        <div class="container text-center">
+            <a href="https://www.facebook.com/batuhanerden" target="_blank"><span class="fa fa-facebook sep grow---"></span></a>
+			<a href="https://www.twitter.com" target="_blank"><span class="fa fa-twitter sep grow---"></span></a>
+			<a href="https://www.linkedin.com/in/batuhan-erden-76686b117?authType=NAME_SEARCH&authToken=6tCO&locale=en_US&trk=tyah&trkInfo=clickedVertical%3Amynetwork%2CclickedEntityId%3A488850558%2CauthType%3ANAME_SEARCH%2Cidx%3A1-1-1%2CtarId%3A1472827096958%2Ctas%3Abatuhan%20erden" target="_blank"><span class="fa fa-linkedin sep grow---"></span></a>
+			<a href="https://github.com/erdenbatuhan/" target="_blank"><span class="fa fa-github sep grow---"></span></a>
+        </div>
+    </nav>
 </body>
-<?php include "Footer.php" ?>
 </html>
--- index.php
+++ index.php
@@ -9,18 +9,29 @@
     <script src="js/bootstrap.js"></script>
 </head>
 <body style="background-color: whitesmoke;">
-        <nav class="navbar navbar-light bg-primary" style="border-radius: 0px; border-bottom-style: groove; border-bottom-color: black; border-bottom-width: 3px;">
-            <div class="container">
-                <a class="navbar-brand" style="color: whitesmoke;" href="http://vailaball.me"> A Vaila Ball </a>
-            </div>
-        </nav>
-        <div class="container text-center">
-            <h4> This site will be constructed soon. For now, it is just used for essential conditions. (Such as; Downloading and Updating the game) </h3>
-            <br><hr style="border-top: 1px solid #3c763d;">
-            <a href="Download" class="btn btn-success btn-lg" role="button" aria-pressed="true"> Download A Vaila Ball </a>
-            <br><hr style="border-top: 1px solid #3c763d;">
+    <nav class="navbar navbar-light navbar-fixed-top bg-primary" style="border-radius: 0px; border-bottom-style: groove; border-bottom-color: black; border-bottom-width: 3px;">
+        <div class="container-fluid">
+            <a class="navbar-brand" style="color: whitesmoke;" href="http://vailaball.me"><h3>A Vai<span>la Ball</span></h3></a>
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <p class="navbar-btn">
+                         <a href="Download" class="btn btn-success btn-sm" role="button" aria-pressed="true"> Download A Vaila Ball </a>
+                    </p>
+                </li>
+            </ul>
         </div>
-        <br><br><br><br><br><br><br><br><br><br>
+    </nav>
+    <div class="container text-center">
+        <br><br><br><br>
+        <h4> This site will be constructed soon. For now, it is just used for essential conditions. (Such as; Downloading and Updating the game) </h3>
+    </div>
+    <nav class="navbar navbar-light navbar-fixed-bottom bg-danger footer" style="border-radius: 0px; padding-top: 10px;">
+        <div class="container text-center">
+            <a href="https://www.facebook.com/batuhanerden" target="_blank"><span class="fa fa-facebook sep grow---"></span></a>
+			<a href="https://www.twitter.com" target="_blank"><span class="fa fa-twitter sep grow---"></span></a>
+			<a href="https://www.linkedin.com/in/batuhan-erden-76686b117?authType=NAME_SEARCH&authToken=6tCO&locale=en_US&trk=tyah&trkInfo=clickedVertical%3Amynetwork%2CclickedEntityId%3A488850558%2CauthType%3ANAME_SEARCH%2Cidx%3A1-1-1%2CtarId%3A1472827096958%2Ctas%3Abatuhan%20erden" target="_blank"><span class="fa fa-linkedin sep grow---"></span></a>
+			<a href="https://github.com/erdenbatuhan/" target="_blank"><span class="fa fa-github sep grow---"></span></a>
+        </div>
+    </nav>
 </body>
-<?php include "Footer.php" ?>
 </html>
