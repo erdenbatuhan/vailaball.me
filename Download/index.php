461a02ece4907fa1eb992f7bf7d261dbd3db0378
erdenbatuhan <erden.batuhan@gmail.com>
2016-09-08 21:22:35 +0300

    'commit'

--- Download/index.php
+++ Download/index.php
@@ -2,16 +2,16 @@
 <html lang="tr">
 <head>
     <title> Download - A Vaila Ball </title>
-    <link rel="stylesheet" href="css/bootstrap.css" type="text/css">
-	<link rel="stylesheet" href="css/styleutiful.css">
+    <link rel="stylesheet" href="../include/css/bootstrap.css" type="text/css">
+	<link rel="stylesheet" href="../include/css/styleutiful.css">
 	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
 	<link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
-    <script src="js/bootstrap.js"></script>
+    <script src="../include/js/bootstrap.js"></script>
 </head>
 <body style="background-color: whitesmoke;">
     <nav class="navbar navbar-light navbar-fixed-top bg-primary" style="border-radius: 0px; border-bottom-style: groove; border-bottom-color: black; border-bottom-width: 3px;">
         <div class="container-fluid">
-            <a class="navbar-brand" style="color: whitesmoke;" href="http://vailaball.me"><h3>A Vai<span>la Ball</span></h3></a>
+            <a class="navbar-brand" style="color: whitesmoke;" href="../"><h3>A Vai<span>la Ball</span></h3></a>
         </div>
     </nav>
     <div class="container text-center">
