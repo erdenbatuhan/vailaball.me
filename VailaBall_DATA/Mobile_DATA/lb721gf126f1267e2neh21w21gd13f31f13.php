5f7709c29868b13725ed127889b0d5f4b8f6a653
erdenbatuhan <erden.batuhan@gmail.com>
2016-09-09 06:13:13 +0300

    '!rank!'

--- VailaBall_DATA/Mobile_DATA/lb721gf126f1267e2neh21w21gd13f31f13.php
+++ VailaBall_DATA/Mobile_DATA/lb721gf126f1267e2neh21w21gd13f31f13.php
@@ -12,6 +12,10 @@
     if (!$connection)
         die("Connection Failed. ". mysqli_connect_error());
 
+    /* ========== VARIABLES FOR USER ========== */
+    $username = $_POST["form_username"];
+    $rank = 0;
+
     /* ========== HASH CODES ========== */
     $unityHash = $_POST["form_hash"];
     $phpHash = "y17rct1y894bt1274128c41n2y7e12he9128bcr7g21wqbuqiubqwpqhex27egc12hr137r";
@@ -21,11 +25,21 @@
     $sql_result = mysqli_query($connection, $sql_query);
 
     // Print Result
-    if ($unityHash != $phpHash && false)
+    if ($unityHash != $phpHash && false) {
         echo "Hash codes won't match up!";
-    else
-        while ($row = mysqli_fetch_assoc($sql_result))
-            echo $row['Username'] . "|" . $row['Score'] . "|";
+    } else {
+        for ($count = 1; $row = mysqli_fetch_assoc($sql_result); $count++) {  
+            if (!strcmp($username, $row['Username']))
+	        $rank = $count;
+
+	    if (count <= 10)	
+	    	echo $row['Username'] . "|" . $row['Score'] . "|";
+	    else if ($rank)
+            	break;
+        }
+
+	echo $rank;
+    }
 
     // Close Connection
     mysqli_close($connection);
